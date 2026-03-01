3d2368dfa0f9010a037fe06cb0892772cfa155bd
dejaaaa <akeo1@paragoniu.edu.kh>
2026-03-01 12:22:30 +0700

    Deploy

---
+++ src/main/java/kh/edu/paragoniu/webworld/WebWorldApplication.java
@@ -0,0 +1,12 @@
+package kh.edu.paragoniu.webworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebWorldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebWorldApplication.class, args);
+    }
+}
---
+++ src/test/java/kh/edu/paragoniu/webworld/WebWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package kh.edu.paragoniu.webworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebWorldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
